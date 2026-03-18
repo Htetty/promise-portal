415fe6040c639f51e02a025a69909ca36ff77a6f
Htetty <htetswork@gmail.com>
2026-03-18 11:14:36 -0700

    corrected feedback link

--- src/app/shared/data/general.tsx
+++ src/app/shared/data/general.tsx
@@ -3,7 +3,7 @@ export const ANNOUNCEMENT_TEXT: string =
   "👋🏽 Tell us what you think about PSP Portal!";
 
 export const ANNOUNCEMENT_LINK: string =
-  "https://forms.gle/iLegLDJcEuSM2aebA";
+  "https://forms.gle/rNSAvE3SMByZ8a8EA";
 
 export const ANNOUNCEMENT_GRAD_COLOR: string =
   "from-[#FDD06E] via-[#FFE8AD]";
